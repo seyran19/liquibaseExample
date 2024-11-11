d1398d1ab3b2e6132d28d84141b7a51a16f4dade
Seiran <s.dzhafarov@astondevs.ru>
2024-11-11 14:51:47 +0300

    First commit

---
+++ src/main/java/com/core/liquibasestart/LiquibaseStartApplication.java
@@ -0,0 +1,13 @@
+package com.core.liquibasestart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquibaseStartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/core/liquibasestart/LiquibaseStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.liquibasestart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
